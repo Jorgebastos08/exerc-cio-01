ec16bb6eba0e4d4b767c7c101261655871da3a88
Jorgebastos08 <90490951+Jorgebastos08@users.noreply.github.com>
2022-04-25 09:04:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Eu me chamo_Jorge Bastos!")
+print("E estou desenvolvendo um trabalho em Python")
+
+print("Exercícios")
+
+n1=int(input("Digite um número:"))
+n2=int(input("Dite mais um número:"))
+s=n1+n2
+print("01 A soma vale", s)
